4aa4790473eb917f6146b04c4b64215565b8287c
Carlos Eduardo <ducameneses16@gmail.com>
2023-03-13 20:16:58 -0300

    Deploy do projeto

---
+++ clientes/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from clientes.models import Cliente
+
+class Clientes(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email','cpf', 'rg', 'celular', 'ativo')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'cpf',)
+    list_filter = ('ativo',)
+    list_editable = ('ativo',)
+    list_per_page = 20
+    ordering = ('nome',)
+
+admin.site.register(Cliente, Clientes)
+
---
+++ clientes/validators.py
@@ -0,0 +1,23 @@
+import re
+from validate_docbr import CPF
+
+def cpf_valido(numero_do_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_do_cpf)
+    
+
+def nome_valido(nome):
+    return nome.isalpha()
+
+def rg_valido(numero_do_rg):
+    return len(numero_do_rg) == 9
+        
+def celular_valido(numero_celular):
+    """Verifica se celular é valido (11 91234-1234)"""
+    modelo = "[0-9]{2} [0-9]{5}-[0-9]{4}"
+    resposta = re.findall(modelo, numero_celular)
+    return resposta
+        
+      
+    
+        
